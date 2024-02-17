604fca0425306fa86e3e20acb2a5b83c074b32a2
lesterpadul <padullester@gmail.com>
2024-02-17 09:39:54 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-https://docs.google.com/presentation/d/1fN0EjV0nLMeimXNYN851SFbheJQrczNhVFBPDWt0zwc/edit#slide=id.p
+web:
+- https://docs.google.com/presentation/d/1fN0EjV0nLMeimXNYN851SFbheJQrczNhVFBPDWt0zwc/edit#slide=id.p
+
+
+qa:
+- https://docs.google.com/presentation/d/1S9qX4ziUnLO67YgjzLjUFJN6fE10SSP_fZ14xeuBWpo/edit#slide=id.g2610876e4b6_0_4
